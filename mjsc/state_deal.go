2aa259a844b9db5fcc526005c43b27b8d56d5c71
chentw <451151014@qq.com>
2025-10-27 21:26:18 +0800

    去掉ani

--- mjsc/state_deal.go
+++ mjsc/state_deal.go
@@ -18,7 +18,6 @@ func (s *StateDeal) OnEnter() {
 	s.game.play.Deal()
 
 	s.game.sender.SendOpenDoorAck()
-	s.game.sender.SendAnimationAck()
 	if s.game.GetRule().GetValue(RuleDiscardTime) != 0 {
 		s.WaitAni(func() { s.game.SetNextState(NewStateSwapTiles) })
 	} else {
