b409504522c7556c648091022a47f0c46bed5561
chentw <451151014@qq.com>
2025-11-12 13:58:57 +0800

    ai

---
+++ trainer/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"strconv"
+	"time"
+
+	"github.com/kevin-chtw/tw_common/gamebase/game"
+	"github.com/kevin-chtw/tw_common/utils"
+	"github.com/kevin-chtw/tw_mjsc_svr/bot"
+	"github.com/kevin-chtw/tw_mjsc_svr/mjsc"
+	"github.com/kevin-chtw/tw_proto/sproto"
+	"github.com/sirupsen/logrus"
+	pitaya "github.com/topfreegames/pitaya/v3/pkg"
+	"github.com/topfreegames/pitaya/v3/pkg/config"
+	"github.com/topfreegames/pitaya/v3/pkg/logger"
+	"github.com/topfreegames/pitaya/v3/pkg/serialize"
+)
+
+var app pitaya.Pitaya
+
+func main() {
+
+	serverType := utils.MJSC
+	pitaya.SetLogger(utils.Logger(logrus.InfoLevel))
+
+	config := config.NewDefaultPitayaConfig()
+	config.SerializerType = uint16(serialize.PROTOBUF)
+	config.Handler.Messages.Compression = false
+	builder := pitaya.NewDefaultBuilder(false, serverType, pitaya.Cluster, map[string]string{}, *config)
+	app = builder.Build()
+
+	defer app.Shutdown()
+
+	logger.Log.Infof("Pitaya server of type %s started", serverType)
+	game.Init(app, mjsc.NewGame, bot.NewPlayer)
+	go train()
+	app.Start()
+}
+
+func train() {
+	time.Sleep(time.Second * 10)
+	table := game.GetTableManager().LoadOrStore(1, 1)
+	table.HandleAddTable(context.Background(), &sproto.AddTableReq{
+		ScoreBase:   1,
+		GameCount:   10000,
+		PlayerCount: 4,
+	})
+	for i := range 4 {
+		table.HandleAddPlayer(context.Background(), &sproto.AddPlayerReq{
+			Playerid: strconv.Itoa(i + 1),
+			Bot:      true,
+			Seat:     int32(i),
+		})
+	}
+}
